9ffb3a99bf1bbe918462975d4802f81606ce1530
Pebloxy <pabvnoob2.0@gmail.com>
2026-03-08 20:40:38 -0300

    implement ticket availability check in purchasing function

--- js/app.js
+++ js/app.js
@@ -9,9 +9,23 @@ function comprar() {
     let quantidadeDesejada = document.getElementById("qtd").value;
     alert(quantidadeDesejada);
     //verifica se há ingressos suficientes disponíveis do tipo especificado
-    
+    let quantidadeSobrando;
+    if (tipoIngresso == "superior") {
+        quantidadeSobrando = parseInt (quantidadeSuperior);
+    } else if (tipoIngresso == "inferior") {
+        quantidadeSobrando = parseInt (quantidadeInferior);
+    } else if (tipoIngresso == "fila") {
+        quantidadeSobrando = parseInt (quantidadePista);
+    } else {
+        alert ("tipo invalido");
+        return;
+    }
+
+    if (parseInt(quantidadeDesejada) > quantidadeSobrando) {
+        alert (`Qantidade indisponível para ${tipoIngresso}`);
+        return;
+    }
 
     //subtrai a quantidade de ingressos disponíveis
-
 
 }
