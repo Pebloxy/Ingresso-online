f3a7052a4699c85e6f3f1cb76c6320331dd930d0
Pebloxy <pabvnoob2.0@gmail.com>
2026-03-08 20:13:40 -0300

    add initial ticket purchasing functionality

---
+++ js/app.js
@@ -0,0 +1,17 @@
+let quantidadeSuperior = document.getElementById("qtd-superior").value;
+let quantidadeInferior = document.getElementById("qtd-inferior").value;
+let quantidadePista = document.getElementById("qtd-pista").value;
+
+function comprar() {
+    //verifica quantos ingressos o usuário deseja comprar e de qual tipo
+    let tipoIngresso = document.getElementById("tipo-ingresso").value;
+    alert(tipoIngresso);
+    let quantidadeDesejada = document.getElementById("qtd").value;
+    alert(quantidadeDesejada);
+    //verifica se há ingressos suficientes disponíveis do tipo especificado
+    
+
+    //subtrai a quantidade de ingressos disponíveis
+
+
+}
