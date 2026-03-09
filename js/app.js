ddfa72f9da30585ca4aab29dc82900b54689b1cd
Pebloxy <pabvnoob2.0@gmail.com>
2026-03-08 21:05:40 -0300

    Pronto primeira versão

--- js/app.js
+++ js/app.js
@@ -1,31 +1,36 @@
-let quantidadeSuperior = document.getElementById("qtd-superior").value;
-let quantidadeInferior = document.getElementById("qtd-inferior").value;
-let quantidadePista = document.getElementById("qtd-pista").value;
-
 function comprar() {
     //verifica quantos ingressos o usuário deseja comprar e de qual tipo
     let tipoIngresso = document.getElementById("tipo-ingresso").value;
-    alert(tipoIngresso);
     let quantidadeDesejada = document.getElementById("qtd").value;
-    alert(quantidadeDesejada);
     //verifica se há ingressos suficientes disponíveis do tipo especificado
     let quantidadeSobrando;
     if (tipoIngresso == "superior") {
-        quantidadeSobrando = parseInt (quantidadeSuperior);
+        quantidadeSobrando = parseInt(document.getElementById("qtd-superior").textContent);
     } else if (tipoIngresso == "inferior") {
-        quantidadeSobrando = parseInt (quantidadeInferior);
-    } else if (tipoIngresso == "fila") {
-        quantidadeSobrando = parseInt (quantidadePista);
+        quantidadeSobrando = parseInt(document.getElementById("qtd-inferior").textContent);
+    } else if (tipoIngresso == "pista") {
+        quantidadeSobrando = parseInt(document.getElementById("qtd-pista").textContent);
     } else {
         alert ("tipo invalido");
         return;
     }
 
     if (parseInt(quantidadeDesejada) > quantidadeSobrando) {
-        alert (`Qantidade indisponível para ${tipoIngresso}`);
+        alert (`Quantidade indisponível para ${tipoIngresso}`);
         return;
+    } else {
+        //subtrai a quantidade de ingressos disponíveis
+        let resultadoDaCompra = quantidadeSobrando - quantidadeDesejada 
+        let quantidadeSuperior = document.getElementById("qtd-superior");
+        let quantidadeInferior = document.getElementById("qtd-inferior");
+        let quantidadePista = document.getElementById("qtd-pista");
+        
+        if (tipoIngresso == "superior") {
+        quantidadeSuperior.innerHTML = resultadoDaCompra;
+      } else if (tipoIngresso == "inferior") {
+        quantidadeInferior.innerHTML = resultadoDaCompra;
+      } else if (tipoIngresso == "pista") {
+        quantidadePista.innerHTML = resultadoDaCompra;
+      }
     }
-
-    //subtrai a quantidade de ingressos disponíveis
-
 }
